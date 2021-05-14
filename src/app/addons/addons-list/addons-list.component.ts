74f6a1ee629cf158fcb478e10d2ec858c10baa56
Jordan C <nykezgaming@live.com>
2021-05-14 11:51:26 -0400

    add static workshop files

--- src/app/addons/addons-list/addons-list.component.ts
+++ src/app/addons/addons-list/addons-list.component.ts
@@ -14,10 +14,20 @@ export class AddonsListComponent implements OnInit {
   addons: GmodstoreUserAddon[];
   user: GmodstoreUser;
 
+  workshopsAddon: any = [
+    { 
+      name: "zz [SL] Anti Adware", description: "This addon blocks some of the adware that is affecting gmod. (the zz are intentional)", url: "https://steamcommunity.com/sharedfiles/filedetails/?id=1881093005", image: "https://steamuserimages-a.akamaihd.net/ugc/1020570173242084166/1DBAB4002227E6522FB884341E023ECDFDAC06A9/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
+    },
+    {
+      name: "StarWars: MTT Drivable", description: "Drivable lua-base vehicle from the movies Star Wars", url: "https://steamcommunity.com/sharedfiles/filedetails/?id=1926964189", image: "https://steamuserimages-a.akamaihd.net/ugc/773986697470074149/04FB5571EFDD45F3524A852901258A0446A13721/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
+    }
+  ];
+
   constructor(private apiConnector: ApiGmsConnectorService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.loadAddons();
+    console.log(this.workshopsAddon);
   }
 
   loadMe() {
@@ -25,6 +35,7 @@ export class AddonsListComponent implements OnInit {
       this.user = res;
     }, error => {
       console.log(error);
+      alert("unable to load gmodstore addons");
     });
   }
 
@@ -34,6 +45,7 @@ export class AddonsListComponent implements OnInit {
       console.log(this.addons);
     }, error => {
       console.log(error);
+      alert("unable to load gmodstore addons");
     })
   }
 
