c482d130ecded5025605d1a4340839bb3b73b5f9
Nykez <nykezgaming@live.com>
2023-03-28 15:39:41 -0400

    Update _configWorkshopAddons.ts

--- src/app/_configWorkshopAddons.ts
+++ src/app/_configWorkshopAddons.ts
@@ -1,13 +1,13 @@
 const workshopAddons: any = [
     { 
         name: "zz [SL] Anti Adware", 
-        description: "This addon blocks some of the adware that is affecting gmod. (the zz are intentional)", 
+        description: "This addon blocks annoying or malicious malware in Garry's Mod. Adlist block is pulled in via HTTP from github.", 
         url: "https://steamcommunity.com/sharedfiles/filedetails/?id=1881093005", 
         image: "https://steamuserimages-a.akamaihd.net/ugc/1020570173242084166/1DBAB4002227E6522FB884341E023ECDFDAC06A9/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
     },
     {
         name: "StarWars: MTT Drivable", 
-        description: "Drivable lua-base vehicle from the movies Star Wars", 
+        description: "Drivable lua-base vehicle (MTT) from the movies Star Wars", 
         url: "https://steamcommunity.com/sharedfiles/filedetails/?id=1926964189", 
         image: "https://steamuserimages-a.akamaihd.net/ugc/773986697470074149/04FB5571EFDD45F3524A852901258A0446A13721/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
     }
